7177de6ca7493ac42f52aeaf0d3946834fb64926
pabasararajapaksha34-byte <pabasara.rajapaksha34@gmail.com>
2026-09-18 19:06:08 +0530

    Add header file for hospital management system

--- lookup.h
+++ lookup.h
@@ -1,1 +1,32 @@
+#ifndef HOSPITAL_H
+#define HOSPITAL_H
 
+#define MAX_SPECIALTIES 4
+#define MAX_WARDS 4
+#define MAX_BEDS 20
+
+/* Doctor Specialty Lookup Table */
+
+extern char specialtyName[MAX_SPECIALTIES][30];
+extern float specialtyFee[MAX_SPECIALTIES];
+extern int specialtyTimePerPatient[MAX_SPECIALTIES];
+extern int specialtyDailyCap[MAX_SPECIALTIES];
+
+/* Hospital Ward Lookup Table */
+
+extern char wardName[MAX_WARDS][40];
+extern float wardDailyRate[MAX_WARDS];
+extern int wardBedCapacity[MAX_WARDS];
+extern int bedOccupancy[MAX_WARDS][MAX_BEDS];
+
+/* Function Prototypes */
+
+void initialiseBedMatrix(void);
+void displaySpecialtyTable(void);
+void displayWardTable(void);
+void displayBedMatrix(void);
+int findFreeBed(int wardIndex);
+void occupyBed(int wardIndex, int bedIndex);
+void freeBed(int wardIndex, int bedIndex);
+
+#endif
