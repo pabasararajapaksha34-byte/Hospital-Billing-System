af562f901735c64b28df1f4dd2ea6f37617a7033
pabasararajapaksha34-byte <pabasara.rajapaksha34@gmail.com>
2026-09-20 13:37:37 +0530

    clear and modified  patient management to sort by priority

--- Patient Order.c
+++ Patient Order.c
@@ -1,61 +1,32 @@
 #include <stdio.h>
-#include <string.h>
+#include "priority.h"
 #include "patient.h"
 
-char patientName[MAX_PATIENTS][MAX_NAME];
-int patientAge[MAX_PATIENTS];
-int patientTriage[MAX_PATIENTS];
-int patientCount = 0;
-
-void addPatient(void)
+void sortAndDisplayByPriority(void)
 {
-    int n;
+    struct Patient temp;
 
-    printf("Enter number of patients: ");
-    scanf("%d", &n);
-
-    for (int i = 0; i < n; i++) {
-        char name[MAX_NAME];
-        int age, triage;
-
-        printf("\nEnter patient %d details\n", i + 1);
-
-        printf("Name: ");
-        scanf("%s", name);
-
-        printf("Age: ");
-        scanf("%d", &age);
-
-        printf("Triage (3=Critical, 2=Urgent, 1=Normal): ");
-        scanf("%d", &triage);
-
-        int position = 0;
-
-        while (position < patientCount && patientTriage[position] >= triage)
-        {
-            position++;
-        }
-        for (int j = patientCount; j > position; j--) {
-            strcpy(patientName[j], patientName[j - 1]);
-            patientAge[j] = patientAge[j - 1];
-            patientTriage[j] = patientTriage[j - 1];
-        }
-        strcpy(patientName[position], name);
-        patientAge[position] = age;
-        patientTriage[position] = triage;
-
-        patientCount++;
+    if (patientCount == 0) {
+        printf("\nNo patients registered.\n");
+        return;
     }
-}
-void displayPatients(void)
-{
-    printf("\n===== PATIENTS IN PRIORITY ORDER =====\n");
-    for (int i = 0; i < patientCount; i++) {
-        printf("%d. %s  Age: %d  ", i + 1, patientName[i], patientAge[i]);
+    for (int i = 0; i < patientCount - 1; i++) {
+        for (int j = i + 1; j < patientCount; j++) {
+            if (patients[j].triage > patients[i].triage) {
 
-        if (patientTriage[i] == 3)
+                temp = patients[i];
+                patients[i] = patients[j];
+                patients[j] = temp;
+            }
+        }
+    }
+    printf("\n===== PATIENTS IN PRIORITY ORDER =====\n");
+
+    for (int i = 0; i < patientCount; i++) {
+        printf("%d. %s  Age: %d  ", i + 1, patients[i].name, patients[i].age);
+        if (patients[i].triage == 3)
             printf("Critical\n");
-        else if (patientTriage[i] == 2)
+        else if (patients[i].triage == 2)
             printf("Urgent\n");
         else
             printf("Normal\n");
