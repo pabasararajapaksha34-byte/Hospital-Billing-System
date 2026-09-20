a53342cad48a109b48daee3b9976b4f1b61f9ad7
pabasararajapaksha34-byte <pabasara.rajapaksha34@gmail.com>
2026-09-20 13:39:42 +0530

    Refactor Output.h for billing functionality
    Removed old billing structure and function declarations for better output.

--- Output.h
+++ Output.h
@@ -1,33 +1,6 @@
-#ifndef BILLING_H
-#define BILLING_H
-#define MAX_SPECIALTIES 4
-#define MAX_WARDS 4
+#ifndef BILL_H
+#define BILL_H
 
-extern char specialty[MAX_SPECIALTIES][30];
-extern float specialtyFee[MAX_SPECIALTIES];
-extern char ward[MAX_WARDS][30];
-extern float wardRate[MAX_WARDS];
-
-struct Patient
-{
-    int id;
-    char name[50];
-    int age;
-    int triage;
-    int specialty;
-    int admitted;
-    int ward;
-    int bed;
-    int days;
-    float surcharge;
-    float wardCost;
-    float total;
-    float discount;
-    float finalAmount;
-};
-
-/* Function declaration */
-
-void printBill(struct Patient p);
+void generateBill(int patientIndex);
 
 #endif
