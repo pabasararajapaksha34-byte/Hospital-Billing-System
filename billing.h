554c3d25f3750e79aa3ff1a1a18220a764db9f8d
pabasararajapaksha34-byte <pabasara.rajapaksha34@gmail.com>
2026-09-20 13:34:44 +0530

    clear billing.h by removing unused data definitions
    Removed specialty and ward data definitions from billing.h.

--- billing.h
+++ billing.h
@@ -1,27 +1,10 @@
 #ifndef BILLING_H
 #define BILLING_H
 
-#define MAX_SPECIALTIES 4
-#define MAX_WARDS 4
-
-/* ---------------- Specialty Data ---------------- */
-
-extern char specialtyName[MAX_SPECIALTIES][30];
-
-extern float specialtyFee[MAX_SPECIALTIES];
-
-extern int specialtyTime[MAX_SPECIALTIES];
-
-
-/* ---------------- Ward Data ---------------- */
-
-extern char wardName[MAX_WARDS][30];
-
-extern float wardRate[MAX_WARDS];
-
-
-/* ---------------- Queue Data ---------------- */
-
-extern int queueCount[MAX_SPECIALTIES];
+float calculateWaitingTime(int specialtyIndex);
+float calculateSurcharge(int specialtyIndex, int triage);
+float calculateWardCost(int wardIndex, int days, int admitted);
+float calculateGrossTotal(float baseFee, float surcharge, float wardCost);
+float calculateDiscount(float grossTotal, int age);
 
 #endif
