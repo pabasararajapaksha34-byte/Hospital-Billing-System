7f98d67ebd91730fec77d00b938379b9c8e913e4
pabasararajapaksha34-byte <pabasara.rajapaksha34@gmail.com>
2026-09-20 16:38:32 +0530

    Refactor bill.c for improved readability

--- bill.c
+++ bill.c
@@ -17,12 +17,30 @@ void generateBill(int patientIndex)
     float waitingTime;
 
     p = patients[patientIndex];
+
     baseFee = specialtyFee[p.specialty];
-    surcharge = calculateSurcharge( p.specialty, p.triage);
-    wardCost = calculateWardCost(p.ward, p.days, p.admitted);
-    grossTotal = calculateGrossTotal(baseFee, surcharge, wardCost);
-    discount = calculateDiscount(grossTotal, p.age);
+
+    surcharge = calculateSurcharge(p.specialty, p.triage);
+
+    wardCost = calculateWardCost(
+        p.ward,
+        p.days,
+        p.admitted
+    );
+
+    grossTotal = calculateGrossTotal(
+        baseFee,
+        surcharge,
+        wardCost
+    );
+
+    discount = calculateDiscount(
+        grossTotal,
+        p.age
+    );
+
     finalAmount = grossTotal - discount;
+
     waitingTime = calculateWaitingTime(p.specialty);
 
     printf("\n====================================\n");
@@ -32,7 +50,10 @@ void generateBill(int patientIndex)
     printf("Patient ID   : %d\n", p.id);
     printf("Name         : %s\n", p.name);
     printf("Age          : %d\n", p.age);
-    printf("Specialty    : %s\n", specialtyName[p.specialty]);
+
+    printf("Specialty    : %s\n",
+           specialtyName[p.specialty]);
+
     printf("Triage       : ");
 
     if (p.triage == 3)
@@ -43,20 +64,43 @@ void generateBill(int patientIndex)
         printf("Normal\n");
 
     if (p.admitted == 1) {
-        printf("Ward         : %s\n", wardName[p.ward]);
-        printf("Bed          : %d\n", p.bed + 1);
-        printf("Days         : %d\n", p.days);
+
+        printf("Ward         : %s\n",
+               wardName[p.ward]);
+
+        printf("Bed          : %d\n",
+               p.bed + 1);
+
+        printf("Days         : %d\n",
+               p.days);
+
     } else {
+
         printf("Ward         : Not Admitted\n");
     }
 
     printf("\n----------- BILL -----------\n");
-    printf("Consultation : LKR %.2f\n", baseFee);
-    printf("Surcharge    : LKR %.2f\n", surcharge);
-    printf("Ward Cost    : LKR %.2f\n", wardCost);
-    printf("Gross Total  : LKR %.2f\n", grossTotal);
-    printf("Discount     : LKR %.2f\n", discount);
-    printf("Final Amount : LKR %.2f\n", finalAmount);
-    printf("Waiting Time : %.2f minutes\n", waitingTime);
+
+    printf("Consultation : LKR %.2f\n",
+           baseFee);
+
+    printf("Surcharge    : LKR %.2f\n",
+           surcharge);
+
+    printf("Ward Cost    : LKR %.2f\n",
+           wardCost);
+
+    printf("Gross Total  : LKR %.2f\n",
+           grossTotal);
+
+    printf("Discount     : LKR %.2f\n",
+           discount);
+
+    printf("Final Amount : LKR %.2f\n",
+           finalAmount);
+
+    printf("Waiting Time : %.2f minutes\n",
+           waitingTime);
+
     printf("============================\n");
 }
