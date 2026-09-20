f96ba5f4bd8e843e9659658c92e4b3fb2406c1df
pabasararajapaksha34-byte <pabasara.rajapaksha34@gmail.com>
2026-09-20 13:28:56 +0530

    Update Patient Data header file structure
    Refactor patient data structure and remove unused variables.

--- Patient Data.h
+++ Patient Data.h
@@ -1,30 +1,23 @@
-#ifndef HOSPITAL_H
-#define HOSPITAL_H
-
+#ifndef PATIENT_H
+#define PATIENT_H
 #define MAX_PATIENTS 100
-#define MAX_SPECIALTIES 4
-#define MAX_WARDS 4
 #define MAX_NAME 50
 
-/* ---------------- Hospital Data ---------------- */
+struct Patient
+{
+    int id;
+    char name[MAX_NAME];
+    int age;
+    int triage;
+    int specialty;
+    int admitted;
+    int ward;
+    int bed;
+    int days;
+};
 
-extern char specialty[MAX_SPECIALTIES][30];
-
-extern char ward[MAX_WARDS][30];
-
-/* ---------------- Patient Data ---------------- */
-
-extern char name[MAX_PATIENTS][MAX_NAME];
-extern int age[MAX_PATIENTS];
-extern int triage[MAX_PATIENTS];
-extern int specialtyID[MAX_PATIENTS];
-extern int admitted[MAX_PATIENTS];
-extern int wardID[MAX_PATIENTS];
-extern int days[MAX_PATIENTS];
-
+extern struct Patient patients[MAX_PATIENTS];
 extern int patientCount;
-
-/* ---------------- Function Declarations ---------------- */
 
 void registerPatient(void);
 void displayPatients(void);
